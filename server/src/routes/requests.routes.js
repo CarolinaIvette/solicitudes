9a9a28d75cb4ddbf0662300b089711590f1e9c15
CarolinaIvette <A00839173@tec.mx>
2026-05-20 11:27:33 -0600

    Práctica 13 completa

--- server/src/routes/requests.routes.js
+++ server/src/routes/requests.routes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router()
 const { isAuthenticated } = require('../middleware/auth')
+const { validate } = require('../middleware/validate')
+const { createRequestSchema, updateRequestSchema } = require('../schemas/requests.schema')
 const { getAll, getById, create, update, remove } = require('../controllers/requests.controller')
 
 router.use(isAuthenticated)
 
 router.get('/', getAll)
 router.get('/:id', getById)
-router.post('/', create)
-router.put('/:id', update)
+router.post('/', isAuthenticated, validate(createRequestSchema), create)
+router.put('/:id', isAuthenticated, validate(updateRequestSchema), update)
 router.delete('/:id', remove)
 
 module.exports = router
+
